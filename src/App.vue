424b2e93baac4492ba8e90b545964a016fd81c72
孙振宇 <sunzhenyu@live.com>
2021-05-27 18:44:45 +0800

    update:remove project existed assets

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-    <router-view v-slot="{ Component }">
-      <Suspense>
-        <component :is="Component" />
-      </Suspense>
-    </router-view>
+    <router-view />
   </div>
 </template>
 
